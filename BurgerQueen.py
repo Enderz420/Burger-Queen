1f805aed2c0243733459644397f4a40f3c369bbb
Enderz <40892919+Enderz420@users.noreply.github.com>
2024-12-04 09:15:41 +0100

    feat: :sparkles: introduce correct login feature, actual main function navigation
    add functionability to main() and add proper user login check.

--- BurgerQueen.py
+++ BurgerQueen.py
@@ -4,33 +4,56 @@
 # En bruker som er ansatt skal kunne se alle ordre
 # En bruker som er ansatt skal kunne se inventaret av ingredienser
 # En bruker som er ansatt skal kunne markere en ordre som fullført, og systemet skal trekke fra brukte ingredienser fra inventaret
-from db import checkUser, loginUser, checkInventory
+from db import loginUser, checkInventory, checkUserAnsettelse, checkUser, checkOrders
 
 isLoggedIn = False # default value
 isAnsatt = False # default value
+username = None # default value
 
 def main():
     print("console.log('break line')")
     print("Velkommen til Burger Queens nye bestillingsportal")
     print("Hva vil du gjøre?")
-    
+    while True:    
+        if not isLoggedIn:
+            Login()
+        else:
+            if isAnsatt:
+                user_input = str(input("Vennligst oppgi valg: "))
+                match user_input:
+                    case "a":
+                        checkUser()
+                    case "b":
+                        listInventory(username)
+                    case _:
+                        break
+            else:
+                user_input = str(input("Vennligst oppgi valg: "))
+                match user_input.lower():
+                    case "a":
+                        Ordre()
+                    case "b":
+                        listInventory(username)
+                    case _:
+                        break
+        
     
 def Ordre():
     print("ordre")
+    checkOrders()
     
     
     
 def Login():
     global isAnsatt
     global isLoggedIn
+    global username
     print("login")
     print("OBS! Case sensitive!")
     username=str(input("Vennligst oppgi brukernavn: "))
-    loginUser(password=str(input("Oppgi passord: ")))
-    if loginUser:
-        checkUser(username)
+    if loginUser(username, password=str(input("Oppgi passord: "))):
         isLoggedIn = True
-        if checkUser:
+        if checkUserAnsettelse(username):
             print("Du er ansatt!")
             isAnsatt = True
         else:
@@ -40,11 +63,16 @@ def Login():
         print("Du har ikke en bruker!")
     
     
-def listInventory():
+def listInventory(username):
     if isLoggedIn & isAnsatt:    
         print("oascnas")
-        checkInventory()
+        checkInventory(username)
     else:
         print("Du har ikke tilgang til dette!")
-checkUser("Geralt")
-listInventory()
+
+checkOrders()
+
+
+if __name__ == "__main__":
+    main()
+    
