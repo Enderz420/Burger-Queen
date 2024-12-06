934f147c804a1f274a7dd4c2f8f8a7008f0eb2ba
Enderz <40892919+Enderz420@users.noreply.github.com>
2024-12-06 13:31:21 +0100

    feat: :sparkles: add methods to add to inventory
    complete method to add ingredient to inventory when called upon
    
    ref #1

--- db.py
+++ db.py
@@ -5,10 +5,10 @@ con = sqlite3.connect("database.db")
 cursor = con.cursor()
 # TODO: Rework file to use classes instead of functions directly.
 ## TODO: Clean up code.
+### TODO: Comments
 
 # ta inn burger, og bruker
 def addOrder(burger, bruker):
-    con = sqlite3.connect("database.db")
     print("add order")
     cursor.execute("INSERT INTO ordre (Hvem, Hva, Produsert) VALUES (?, ?, ?)", (bruker, burger, 0,))
     con.commit()
@@ -22,22 +22,49 @@ def addOrder(burger, bruker):
         ordre_id, hvem, hva, produsert = new_order
         if produsert == 1:
             produsert = "Ja"
-
         produsert = "Nei"
         print(f"Nyeste bestilling: Bestillingsnummer: {ordre_id}\n Bestiller: {hvem}\n Burger: {hva}\n Ferdig? {produsert}")
         deductIngredienser(burger)
-        if is_order_completed():
+        
+        match produsert:
+            case "Ja":
+                produsert = 1
+            case "Nei":
+                produsert = 0
+            case _:
+                pass
+        if is_order_completed(produsert):
             print("Bestilling er ferdig!")
         else:
             print("Nyeste bestilling er ikke ferdig enda.")
     else:
         print("Ingen bestilling funnet")
 
+def removeOrder(ID, produsert): # TODO: Add ingredients on order removal if it's done
+    try:
+        con = sqlite3.connect("database.db")
+        print("remove order")
+        
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM ordre WHERE ID = ?", (ID,))
+        result = cursor.fetchone()[0]
+        
+        print(result) 
+        if result == ID: 
+            cursor.execute("DELETE FROM ordre WHERE ID = ?", (ID,))
+            con.commit()
+            cursor.execute("SELECT * FROM ordre")
+        else:
+            print("Could not handle")
+        con.close()
+    except sqlite3.Error as e:
+        print(f"{e}")
+
 def is_order_completed(status):
     ORDER_COMPLETED = 1
     return status == ORDER_COMPLETED
 
-def deductIngredienser(burger): # TODO Fix database writes, so that it doesn't throw database is locked operational error.
+def deductIngredienser(burger):
     con = sqlite3.connect("database.db")
     cursor = con.cursor()
     
@@ -53,11 +80,33 @@ def deductIngredienser(burger): # TODO Fix database writes, so that it doesn't t
 
         if int(current_quantity) >= amount_to_deduct:
             cursor.execute("UPDATE Ingredienser SET HvorMye = HvorMye - ? WHERE ID = ?", (amount_to_deduct, ingrediens))
-            print("remove 1")
+            print(f"remove one {ingrediens}")
 
         else:
             print(f"Du har ikke nokk {ingrediens} til å lage dette!")
         
+    con.commit()
+    
+    con.close()
+    
+def addIngredienser(burger):
+    con = sqlite3.connect("database.db")
+    cursor = con.cursor()
+    
+    cursor.execute("SELECT IngrediensID FROM BurgerHasIngredienser WHERE BurgerID = ?", (burger,))
+    ingredienser = cursor.fetchall()
+
+    for g_ingrediens in ingredienser:
+        ingrediens = g_ingrediens[0]
+
+        cursor.execute("SELECT HvorMye FROM Ingredienser WHERE ID = ?", (ingrediens,))
+        current_quantity = cursor.fetchone()[0]
+        amount_to_add = 1 
+
+        if int(current_quantity) >= amount_to_add:
+            cursor.execute("UPDATE Ingredienser SET HvorMye = HvorMye + ? WHERE ID = ?", (amount_to_add, ingrediens))
+            print(f"add one {ingrediens}")
+                    
     con.commit()
     
     con.close()
@@ -115,6 +164,3 @@ def checkOrders():
     for row in rows:
         print(f"{row}\n", sep='-')
         
-checkOrders()
-
-addOrder(1, "Geralt")
