dda5f64915a77cffff9a4bfe2b302aaf1adf4639
Enderz <40892919+Enderz420@users.noreply.github.com>
2024-12-12 11:33:39 +0100

    feat: :sparkles: add import for sys, time and os, create logo for burger queen and partially finish detailing interactive menu.
    add import for sys, time and os, create logo for burger queen and partially finish detailing interactive menu. Add User() function to get user to create an account.

--- BurgerQueen.py
+++ BurgerQueen.py
@@ -4,64 +4,88 @@
 # En bruker som er ansatt skal kunne se alle ordre
 # En bruker som er ansatt skal kunne se inventaret av ingredienser
 # En bruker som er ansatt skal kunne markere en ordre som fullført, og systemet skal trekke fra brukte ingredienser fra inventaret
-from db import loginUser, checkInventory, checkUserAnsettelse, checkUser, checkOrders, addOrder, removeOrder
+from db import loginUser, checkInventory, checkUserAnsettelse, checkUser, checkOrders, addOrder, removeOrder, createUser, completeOrder
+from os import get_terminal_size
+import time
+import sys
 
 isLoggedIn = False # default value
 isAnsatt = False # default value
 username = None # default value
 
+
 def main(): # TODO: Format output to be more prettier
-    print("console.log('break line')")
+    term_size = get_terminal_size()
+    print('=' * term_size.columns)
+    font = """
+            $$$$$$$\                                                           $$$$$$\                                          
+            $$  __$$\                                                         $$  __$$\                                         
+            $$ |  $$ |$$\   $$\  $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\        $$ /  $$ |$$\   $$\  $$$$$$\   $$$$$$\  $$$$$$$\  
+            $$$$$$$\ |$$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\       $$ |  $$ |$$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\ 
+            $$  __$$\ $$ |  $$ |$$ |  \__|$$ /  $$ |$$$$$$$$ |$$ |  \__|      $$ |  $$ |$$ |  $$ |$$$$$$$$ |$$$$$$$$ |$$ |  $$ |
+            $$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |$$   ____|$$ |            $$ $$\$$ |$$ |  $$ |$$   ____|$$   ____|$$ |  $$ |
+            $$$$$$$  |\$$$$$$  |$$ |      \$$$$$$$ |\$$$$$$$\ $$ |            \$$$$$$ / \$$$$$$  |\$$$$$$$\ \$$$$$$$\ $$ |  $$ |
+            \_______/  \______/ \__|       \____$$ | \_______|\__|             \___$$$\  \______/  \_______| \_______|\__|  \__|
+                                        $$\   $$ |                               \___|                                        
+                                        \$$$$$$  |                                                                            
+                                        \______/                                                                             
+            """
+    for s in font:
+        sys.stdout.write(s)
+        sys.stdout.flush()
+        time.sleep(0.001)
+    print('=' * term_size.columns)
     print("Velkommen til Burger Queens nye bestillingsportal")
     print("Hva vil du gjøre?")
     while True:    
         if not isLoggedIn:
             Login()
         if isAnsatt:
-            user_input = str(input("Vennligst oppgi valg: "))
+            print("""
+            Meny: 
+                1: Sjekk aktive brukere 
+                2: Vis lager
+                3: Ordre """)
+
+            user_input = int(input("Vennligst oppgi valg: "))
             match user_input:
-                case "a":
+                case "1":
                     checkUser()
-                case "b":
+                case "2":
                     listInventory(username)
+                case "3":
+                    Ordre()
                 case _:
                     break
         else:
-            user_input = str(input("Vennligst oppgi valg: "))
-            match user_input.lower():
-                case "a":
-                    Ordre()
-                case "b":
-                    listInventory(username)
+            user_input = int(input("Vennligst oppgi valg: "))
+            match user_input:
+                case "1":
+                    addOrder(burger=int(input("Hvilken burger vil du bestille?")), username=username)
                 case _:
                     break
         
     
-def Ordre(): # TODO: Remove later
+def Ordre():
     print("ordre")
-    print("Hva vil du gjøre?")
-    if isAnsatt:
-        user_input = str(input("Vennligst oppgi valg: "))
-        match user_input:
-            case "a":
-                checkUser()
-            case "b":
-                listInventory(username)
-            case "c":
-                checkOrders(username)
-                removeOrder(ID=int(input("Hvilken ordre vil du fjerne")))
-            case _:
-                pass
-    else:
-        user_input = str(input("Vennligst oppgi valg: "))
-        match user_input.lower():
-            case "a":
-                addOrder(burger=int(input("Hva vil du bestille?")), username=username)
-            case "b":
-                listInventory(username)
-            case _:
-                pass
-    
+    print("""
+          Valg: 
+            1: Sjekk bestillinger på en bruker 
+            2: Marker en bestilling som ferdig for bruker 
+            3: Fjerne bestilling !!! Gjør dette etter du har markert en bestilling som ferdig !!! """)
+
+    user_input = str(input("Vennligst oppgi valg: "))
+    match user_input:
+        case "1":
+            checkOrders(username)
+        case "2":
+            checkOrders(username)
+            completeOrder(ID=int(input("Hvilken ordre er du ferdig med?")))
+        case "3":
+            checkOrders(username)
+            removeOrder(ID=int(input("Hvilken ordre vil du fjerne")))
+        case _:
+            pass
     
     
 def Login():
@@ -71,6 +95,10 @@ def Login():
     print("login")
     print("OBS! Case sensitive!")
     username=str(input("Vennligst oppgi brukernavn: "))
+    if checkUser(username):
+        print("Du har ikke bruker!")
+        print("Du må lage en bruker for å bruke programmet")
+        User()
     if loginUser(username, password=str(input("Oppgi passord: "))):
         isLoggedIn = True
         if checkUserAnsettelse(username):
@@ -82,6 +110,11 @@ def Login():
     else:
         print("Du har ikke en bruker!")
     
+def User():
+    global username
+    print("Registrer ny bruker")
+    username = str(input("Vennligst oppgi et unikt brukernavn"))
+    createUser(username, password=str(input("Vennligst oppgi et passord som du husker!")))
     
 def listInventory(username): # TODO: Remove later
     if isLoggedIn & isAnsatt:    
